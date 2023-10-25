415c700d4bcd9a3bdc13b037d8bb54af63a3ac1a
Uday <gadikaruday28@gmail.com>
2023-10-25 18:07:28 +0530

    added getTestCaseForText func inside backend and added functionality for generating test for text PRD's

--- UI.py
+++ UI.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import time
 
 # ----Package to load imageFile---#
 from PIL import Image
@@ -7,9 +8,7 @@ from PIL import Image
 # ---Package to read Docx file---#
 import docx2txt
 
-# ---Package to read PDF file---#
-import PyPDF2
-
+import backend
 
 # ----------Function to load images-----------------#
 def load_image(image_file):
@@ -21,64 +20,70 @@ def main():
     with st.sidebar:
         selected = option_menu(
             menu_title="Main Menu",
-            options=["DocumentFiles", "Text", "ImageFiles"],
+            options=["DocumentFiles", "Text"],
         )
-
-    # -----c--------------------------------------DocumentFiles Menu----------------------------------------------------#
+    testCases = ""
+    # -------------------------------------------DocumentFiles Menu----------------------------------------------------#
     if selected == "DocumentFiles":
         st.subheader(selected)
         file = st.file_uploader("Upload Document", type=["pdf", "docx", "txt"])
         if st.button("Process"):
             if file is not None:
+
                 # --------------Process TextFile-------------------#
                 if file.type == 'text/plain':
                     raw_text = file.read()
                     raw_text = str(file.read(), "utf-8")
-                    st.write("Preview")
+                    st.write("Possible TestCases")
+                    # with st.spinner('Wait for it...'):
+                    #     time.sleep(100)
                     st.write(raw_text)
                 # --------------Process PdfFile-------------------#
                 elif file.type == "application/pdf":
                     try:
-                        st.write("Preview")
-                        pdf_reader = PyPDF2.PdfReader(file)
-                        text = ""
-                        for i in range(len(pdf_reader.pages)):
-                            page = pdf_reader.pages[i]
-                            text += page.extract_text()
-                        st.write(text)
+                        st.write("Possible TestCases")
+                        # with st.spinner('Wait for it...'):
+                        #     time.sleep(100)
+                        testCases = backend.getTestCasesForPdfPrd(file)
+                        st.write(testCases)
                     except:
                         st.write("None")
                 # --------------Process DocFile-------------------#
                 else:
                     raw_text = docx2txt.process(file)
-                    st.write("Preview")
+                    st.write("Possible TestCases")
+                    # with st.spinner('Wait for it...'):
+                    #     time.sleep(100)
                     st.write(raw_text)
-
 
 
     # -------------------------------------------Text Menu---------------------------------------------------------#
     elif selected == "Text":
         st.subheader(selected)
         txt = st.text_area(
-            label="Provide Input here",
+            label="Provide PRD as a text here",
             height=200,
             max_chars=10000,
             placeholder="Write here...."
         )
         if st.button("Process"):
-            st.write("Preview")
-            st.write(txt)
+            st.write("Possible TestCases")
+            # with st.spinner('Wait for it...'):
+            #     time.sleep(100)
+            testCases = backend.getTestCasesForTextPrd(txt)
+            st.write(testCases)
+
 
 
 
     # ------------------------------------------ImageFiles Menu-----------------------------------------------------#
-    elif selected == "ImageFiles":
-        st.subheader(selected)
-        image_file = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
-        if st.button("Process"):
-            st.write("Preview")
-            if image_file:
-                st.image(load_image(image_file))
+    # elif selected == "ImageFiles":
+    #     st.subheader(selected)
+    #     image_file = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
+    #     if st.button("Process"):
+    #         st.write("Preview")
+    #         if image_file:
+    #             st.image(load_image(image_file))
 
 
 if __name__ == '__main__':
---
+++ backend.py
@@ -0,0 +1,82 @@
+from dotenv import load_dotenv
+from pypdf import PdfReader
+import os
+import time
+import openai
+
+load_dotenv()
+
+
+# ------------------Generation of TestCase via uploading pdf file----------------------------------- #
+def getTestCasesForPdfPrd(pdfFileObject):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    pdfReader = PdfReader(pdfFileObject)
+    text = []
+    testCases = ''
+
+    for i in range(0, len(pdfReader.pages)):
+        pageObj = pdfReader.pages[i].extract_text()
+        pageObj = pageObj.replace('\t\r', '')
+        pageObj = pageObj.replace('\xa0', '')
+        text.append(pageObj)
+
+    def joinMultiplePages(lst, pages):
+        newText = []
+        for i in range(0, len(lst), pages):
+            newText.append(''.join(lst[i:i + pages]))
+        return newText
+
+    newText = joinMultiplePages(text, 3)
+
+    def getAnswer(prompt, model="gpt-3.5-turbo"):
+        messages = [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=0,  # this is the degree of randomness of the model's output
+        )
+        return response.choices[0].message["content"]
+
+    for i in range(0, len(newText)):
+        prompt = f"""Provide me the test cases which should be tagged based on the product feature,priority,severity and to be collected in respective test suites for given Product Requirement Document.
+        I will give you content of the same from each pages, beginning to end.
+        Don't be conversational. Content is shared below, delimited with triple backticks:
+        ```{newText[i]}```
+        """
+        try:
+            response = getAnswer(prompt)
+        except:
+            response = getAnswer(prompt)
+        print(response)
+        testCases = testCases + ' ' + response + '\n\n'
+        time.sleep(19)
+    return testCases
+
+
+# ------------------Generation of TestCase via uploading using text file----------------------------------- #
+def getTestCasesForTextPrd(txtfile):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    def getAnswer(prompt, model="gpt-3.5-turbo"):
+        messages = [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=0,  # this is the degree of randomness of the model's output
+        )
+        return response.choices[0].message["content"]
+
+    prompt = f"""Provide me the test cases which should be tagged based on the product feature,priority,severity and to be collected in respective test suites for given Product Requirement Document.
+    I will give you content of the same from each pages, beginning to end.
+    Don't be conversational. Content is shared below, delimited with triple backticks:
+    ```{txtfile}```
+    """
+    try:
+        response = getAnswer(prompt)
+    except:
+        response = getAnswer(prompt)
+    testCases = ""
+    print(response)
+    testCases = testCases + ' ' + response + '\n\n'
+    time.sleep(19)
+    return testCases
